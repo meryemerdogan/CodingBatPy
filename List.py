638afacefd426700cd6b3efa5d0052718adf1531
Meryem Erdoğan <meryemerdogan04@gmail.com>
2024-03-28 12:09:06 +0300

    Lists 2 added
    Solutions to lists-2

--- List.py
+++ List.py
@@ -60,3 +60,63 @@ def make_ends(nums):
 
 def has23(nums):
   return nums[0] == 2 or nums[0] == 3 or nums[1] == 2 or nums[1] == 3
+
+############################################
+############### lists-2 ###################
+############################################
+
+def count_evens(nums):
+  count = 0
+  for i in nums:
+    if i%2 == 0:
+      count += 1
+  return count
+
+def big_diff(nums):
+  minimum = nums[0]
+  maximum = nums[0]
+  for i in nums:
+    minimum =  min( minimum, i)
+    maximum =  max( maximum, i)
+  diff = maximum -minimum
+  return diff
+
+def centered_average(nums):
+  s = nums[0]
+  b = nums[0]
+  total = 0
+  
+  for i in nums:
+    s = min (s, i)
+    b = max(b, i)
+    total += i
+  return (total - b - s) / (len(nums)-2)
+
+def sum13(nums):
+  total = 0
+  i = 0
+  while i < len(nums):
+    if nums[i] == 13:
+      i += 2
+    else:
+      total += nums[i]
+      i += 1
+  return total
+
+def sum67(nums):
+  add = True
+  t = 0
+  for i in nums:
+    if i == 6:
+      add = False
+    elif not add and i == 7:
+      add = True
+    elif add:
+      t += i
+  return t
+
+def has22(nums):
+  for i in range(len(nums)-1):
+    if nums[i] == 2 and nums[i+1] == 2:
+      return True
+  return False
